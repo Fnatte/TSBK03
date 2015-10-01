be3cc9f503acaadfc5557ad107ee21292fac6686
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-10-01 15:01:47 +0200

    Corrected all wierd characters.

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -4,12 +4,12 @@
 // OpenGL 3 conversion 2013.
 
 #ifdef __APPLE__
-	#include <OpenGL/gl3.h>
-	#include "MicroGlut.h"
-	// uses framework Cocoa
+#include <OpenGL/gl3.h>
+#include "MicroGlut.h"
+// uses framework Cocoa
 #else
-	#include <GL/gl.h>
-	#include "MicroGlut.h"
+#include <GL/gl.h>
+#include "MicroGlut.h"
 #endif
 
 #include <stdlib.h>
@@ -17,53 +17,49 @@
 #include "SpriteLight.h"
 #include "GL_utilities.h"
 
-// Lägg till egna globaler här efter behov.
+// L√§gg till egna globaler h√§r efter behov.
 
 
-void SpriteBehavior() // Din kod!
-{
-// Lägg till din labbkod här. Det går bra att ändra var som helst i
-// koden i övrigt, men mycket kan samlas här. Du kan utgå från den
-// globala listroten, gSpriteRoot, för att kontrollera alla sprites
-// hastigheter och positioner, eller arbeta från egna globaler.
+void SpriteBehavior() {
+	// L√§gg till din labbkod h√§r. Det g√•r bra att √§ndra var som helst i
+	// koden i √∂vrigt, men mycket kan samlas h√§r. Du kan utg√• fr√•n den
+	// globala listroten, gSpriteRoot, f√∂r att kontrollera alla sprites
+	// hastigheter och positioner, eller arbeta fr√•n egna globaler.
 }
 
 // Drawing routine
-void Display()
-{
+void Display() {
 	SpritePtr sp;
-	
+
 	glClearColor(0, 0, 0.2, 1);
 	glClear(GL_COLOR_BUFFER_BIT+GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	
+
 	DrawBackground();
-	
+
 	SpriteBehavior(); // Din kod!
-	
-// Loop though all sprites. (Several loops in real engine.)
+
+	// Loop though all sprites. (Several loops in real engine.)
 	sp = gSpriteRoot;
 	do
-	{
-		HandleSprite(sp); // Callback in a real engine
-		DrawSprite(sp);
-		sp = sp->next;
-	} while (sp != NULL);
-	
+		{
+			HandleSprite(sp); // Callback in a real engine
+			DrawSprite(sp);
+			sp = sp->next;
+		} while (sp != NULL);
+
 	glutSwapBuffers();
 }
 
-void Reshape(int h, int v)
-{
+void Reshape(int h, int v) {
 	glViewport(0, 0, h, v);
 	gWidth = h;
 	gHeight = v;
 }
 
-void Timer(int value)
-{
+void Timer(int value) {
 	glutTimerFunc(20, Timer, 0);
 	glutPostRedisplay();
 }
@@ -76,7 +72,7 @@ void Key(unsigned char key,
          __attribute__((unused)) int y)
 {
   switch (key)
-  {
+		{
     case '+':
     	someValue += 0.1;
     	printf("someValue = %f\n", someValue);
@@ -87,20 +83,19 @@ void Key(unsigned char key,
     	break;
     case 0x1b:
       exit(0);
-  }
+		}
 }
 
-void Init()
-{
+void Init() {
 	TextureData *sheepFace, *blackFace, *dogFace, *foodFace;
-	
+
 	LoadTGATextureSimple("bilder/leaves.tga", &backgroundTexID); // Bakgrund
-	
-	sheepFace = GetFace("bilder/sheep.tga"); // Ett får
-	blackFace = GetFace("bilder/blackie.tga"); // Ett svart får
+
+	sheepFace = GetFace("bilder/sheep.tga"); // Ett f√•r
+	blackFace = GetFace("bilder/blackie.tga"); // Ett svart f√•r
 	dogFace = GetFace("bilder/dog.tga"); // En hund
 	foodFace = GetFace("bilder/mat.tga"); // Mat
-	
+
 	NewSprite(sheepFace, 100, 200, 1, 1);
 	NewSprite(sheepFace, 200, 100, 1.5, -1);
 	NewSprite(sheepFace, 250, 200, -1, 1.5);
@@ -113,15 +108,15 @@ int main(int argc, char **argv)
 	glutInitWindowSize(800, 600);
 	glutInitContextVersion(3, 2);
 	glutCreateWindow("SpriteLight demo / Flocking");
-	
+
 	glutDisplayFunc(Display);
 	glutTimerFunc(20, Timer, 0); // Should match the screen synch
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Key);
-	
+
 	InitSpriteLight();
 	Init();
-	
+
 	glutMainLoop();
 	return 0;
 }
